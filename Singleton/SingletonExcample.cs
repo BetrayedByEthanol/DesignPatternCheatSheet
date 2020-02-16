eb692dca5ddc4c2395901a8698dace2fdcc15586
JS_Ra <Me>
2020-02-16 10:58:05 +0100

    Add project files.

---
+++ Singleton/SingletonExcample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternCheatSheet.Singleton
+{
+    public class SingletonExcample
+    {
+
+        private static SingletonExcample instance;
+        private SingletonExcample(string param)
+        {
+
+        }
+
+        private SingletonExcample(int param)
+        {
+
+        }
+
+        public static SingletonExcample getInstance()
+        {
+            return instance;
+        }
+    }
+}
