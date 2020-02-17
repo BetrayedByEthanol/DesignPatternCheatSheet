64e5247722571db5f6e4bb07d6ff179d2ea54495
19nieke <JS_Ram38@hotmail.com>
2020-02-17 15:47:58 +0100

    more stuff PepeHands

---
+++ CreationPatterns/FluentInterface/FluentInterfaceExample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternCheatSheet.CreationPatterns.FluentInterface
+{
+    class FluentInterfaceExample
+    {
+    }
+}
---
+++ StructuralPatterns/PrivateClassData/PrivateClassDataExample.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternCheatSheet.StructuralPatterns.PrivateClassData
+{
+    class PrivateClassDataExample
+    {
+    }
+}
